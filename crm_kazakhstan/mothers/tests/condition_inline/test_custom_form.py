2a0b846f78f4ae2191c1c5bec91a83206bbd776f
rodionmaulenov <natalia.maulenova@gmail.com>
2023-12-29 20:41:21 +0200

    write test for init method condition inline

--- crm_kazakhstan/mothers/tests/condition_inline/test_custom_form.py
+++ crm_kazakhstan/mothers/tests/condition_inline/test_custom_form.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
 from django.test import TestCase, RequestFactory
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth import get_user_model
 from django.db import models
 
-from mothers.models import Mother
+from mothers.models import Mother, Condition
 from mothers.inlines import ConditionInline
 
 User = get_user_model()
@@ -65,3 +66,54 @@ class CleanMethodConditionInlineFormWithoutFinishedTest(TestCase):
 
         with self.assertRaises(KeyError):
             self.assertNotIn('Date must be provided if time is set.', formset_instance.errors[0]['scheduled_date'])
+
+
+class InitMethodConditionInlineFormWithoutFinished(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_superuser('admin', 'admin@example.com', 'password')
+
+    def test_when_finished_equal_True_change_not_allowed(self):
+        mother_instance = Mother.objects.create(name="Test")
+        Condition.objects.create(mother=mother_instance, condition='WWW', reason=None,
+                                 scheduled_date=None, finished=True)
+
+        request = self.factory.get('/')
+        request.user = self.user
+
+        # Get the inline formset class
+        inline = ConditionInline(Mother, admin.site)
+        formset_class = inline.get_formset(request, mother_instance)
+
+        # Create a formset instance for the existing mother instance
+        formset = formset_class(instance=mother_instance)
+
+        # Use _construct_form to create a form
+        form = formset._construct_form(0)
+
+        for field_name, instance in form.fields.items():
+            if field_name not in ['id', 'DELETE', 'mother']:
+                self.assertTrue(form.fields[field_name].disabled)
+
+
+    def test_when_finished_equal_True_change_allowed(self):
+        mother_instance = Mother.objects.create(name="Test")
+        Condition.objects.create(mother=mother_instance, condition='WWW', reason=None,
+                                 scheduled_date=None, finished=False)
+
+        request = self.factory.get('/')
+        request.user = self.user
+
+        # Get the inline formset class
+        inline = ConditionInline(Mother, admin.site)
+        formset_class = inline.get_formset(request, mother_instance)
+
+        # Create a formset instance for the existing mother instance
+        formset = formset_class(instance=mother_instance)
+
+        # Use _construct_form to create a form
+        form = formset._construct_form(0)
+
+        for field_name, instance in form.fields.items():
+            if field_name not in ['id', 'DELETE', 'mother']:
+                self.assertFalse(form.fields[field_name].disabled)
